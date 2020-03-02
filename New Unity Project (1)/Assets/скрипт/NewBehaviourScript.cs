7a35cdedf1148143eaded0b1fa285090c8e1c44b
Касаткин Владимир Владимирович <v.v.kasatkin@academy.local>
2020-03-02 20:04:11 +0400

    Add cube

---
+++ New Unity Project (1)/Assets/скрипт/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public float speed = 20f;
+	private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+	float moveX = Input.GetAxis("Horizontal");
+	transform.Translate(Vector3.left * moveX * Time.deltaTime);
+
+        float moveY = Input.GetAxis("Vertical");
+	transform.Translate(Vector3.up * moveY * 5.0f * Time.deltaTime);
+	}
+}
